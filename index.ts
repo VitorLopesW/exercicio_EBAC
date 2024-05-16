ade8c0833edf6ecc191d7fe165ad6a0926fdd5ea
Vitor Lopes <67442259+VitorLopesW@users.noreply.github.com>
2024-05-16 17:17:51 -0300

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,11 @@
+function soma(a: number, b: number): number {
+    return a + b
+}
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`
+}
+
+console.log(soma(1, 2))
+
+console.log(saudacao('Fulano'))
